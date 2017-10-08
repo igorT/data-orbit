e21c13e31aa2a160594d2dbe11f418d269259055
Igor Terzic <terzicigor@gmail.com>
2017-10-09 02:23:14 +0300

    progress

--- addon/-private/system/relationships/belongs-to.js
+++ addon/-private/system/relationships/belongs-to.js
@@ -125,14 +125,15 @@ export default function belongsTo(modelName, options) {
       if (value === undefined) {
         value = null;
       }
+      /*
       if (value && value.then) {
         this._internalModel._relationships.get(key).setRecordPromise(value);
       } else if (value) {
         this._internalModel._relationships.get(key).setInternalModel(value._internalModel);
       } else {
         this._internalModel._relationships.get(key).setInternalModel(value);
-      }
-
+      }*/
+      this._internalModel.setBelongsTo(key, value);
       return this._internalModel._relationships.get(key).getRecord();
     }
   }).meta(meta);
